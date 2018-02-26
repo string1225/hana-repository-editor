9423ddb5234f4eec7d8efe52c761c755e5057a1e
String Shi <string.shi@sap.com>
2018-02-26 11:11:25 +0800

    Change to TS Format

--- src/hana.ts
+++ src/hana.ts
@@ -2,10 +2,45 @@
 
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-const template = require('./hanaTemplate');
+import { template } from './template';
+import "jquery/jquery.d.ts"
+// const template = require('./hanaTemplate');
+
+export default class HanaConnector {
+
+    static tokenApi = '/sap/hana/xs/dt/base/server/csrf.xsjs';
+    static infoApi = '/sap/hana/xs/dt/base/info';
+    static fileApi = '/sap/hana/xs/dt/base/file';
+    static transferApi = '/sap/hana/xs/dt/base/xfer/import';
+
+    private _ip: string;
+    private _instanceNo: string;
+    private _username: string;
+    private _password: string;
+    private _token: string;
+    constructor() {
+
+    };
+    private static _getToken(): Promise<string> {
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                url: this.tokenApi,
+                type: 'HEAD',
+                headers: { "X-CSRF-Token": "Fetch" },
+                success: (data, textStatus, jqXHR) => {
+                    if (jqXHR.getResponseHeader("x-sap-login-page")) {
+                        return;
+                    }
+                    this._token = jqXHR.getResponseHeader("X-CSRF-Token");
+                    resolve(this._token);
+                }
+            });
+        });
+    }
+}
 
 function Hana() {
-   
+
 }
 
 Hana.prototype = {
